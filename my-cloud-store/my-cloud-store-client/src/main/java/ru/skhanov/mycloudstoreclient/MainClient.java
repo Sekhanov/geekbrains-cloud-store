98cb3d00f51714deb67af08085c44defd127df62
s.khanov <sekhanov@gmail.com>
2019-01-23 18:30:27 +0300

    signUp

--- my-cloud-store/my-cloud-store-client/src/main/java/ru/skhanov/mycloudstoreclient/MainClient.java
+++ my-cloud-store/my-cloud-store-client/src/main/java/ru/skhanov/mycloudstoreclient/MainClient.java
@@ -9,14 +9,14 @@ import javafx.stage.Stage;
 public class MainClient extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/signUp.fxml"));
         Parent root = fxmlLoader.load();
         primaryStage.setTitle("Box Client");
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
