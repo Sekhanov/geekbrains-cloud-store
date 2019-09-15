145c5e34c24c8db300357246a77ca87409e109f1
Sergey Khanov (home) <sekhanov@gmail.com>
2019-09-15 10:22:26 +0300

    authentication proxy

--- my-cloud-store/my-cloud-store-server/src/main/java/ru/skhanov/mycloudstoreserver/service/AuthenticationFactory.java
+++ my-cloud-store/my-cloud-store-server/src/main/java/ru/skhanov/mycloudstoreserver/service/AuthenticationFactory.java
@@ -6,6 +6,6 @@ package ru.skhanov.mycloudstoreserver.service;
 public class AuthenticationFactory {
 
     public static AuthenticationService createAuthenticationService() {
-        return new SqlUsersDaoService("jdbc:sqlite:my_cloud_store_server.db", "org.sqlite.JDBC");
+        return new SqlDaoServiceLoggingProxy();
     }
 }
